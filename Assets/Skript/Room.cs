5207cf25f3d21334df9a9d7f5dac404dc786dc1f
Danila Komar <danila.komar04@mail.ru>
2023-06-08 00:47:57 +0300

    3.1 generation commit

---
+++ Assets/Skript/Room.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject DoorU;
+    public GameObject DoorR;
+    public GameObject DoorD;
+    public GameObject DoorL;
+
+    public Mesh[] BlockMeshes;
+
+    private void Start()
+    {
+        
+    }
+
+    public void RotateRandomly()
+    {
+        
+    }
+}
